aab4557ed1a5b918a34de1e28375cb86e84577db
jloveben <99923410z>
2017-11-29 10:01:59 +0800

    1.2版

--- website/web/js/main.js
+++ website/web/js/main.js
@@ -1,22 +1,40 @@
 jQuery(function($) {
-
-	//Ajax contact
-	var form = $('.contact-form');
-		form.submit(function () {
-			$this = $(this);
-			$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
+	//右侧咨询
+	$(".zx li").mouseenter(function(){
+		$(this).children("div").show();
+	})
+	$(".zx li").mouseleave(function(){
+		$(this).children("div").stop().hide();
+	})	
+	$(window).scroll(function(){
+		var A = $(window).scrollTop();
+	
+		if(A > 400){
+			$("#back").css("visibility","visible");
+		}else{
+			$("#back").css("visibility","hidden");
+		}
 	});
-
-	//Goto Top
-	$('.gototop').click(function(event) {
-		 event.preventDefault();
-		 $('html, body').animate({
-			 scrollTop: $("body").offset().top
-		 }, 500);
-	});	
-	//End goto top		
-
+	$("#back").click(function(){
+		$("html,body").animate(
+			{
+				"scrollTop":0
+			},500
+		);	
+	});
+	//美洽
+	
 });
+(function(m, ei, q, i, a, j, s) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        j = ei.createElement(q),
+            s = ei.getElementsByTagName(q)[0];
+        j.async = true;
+        j.charset = 'UTF-8';
+        j.src = 'https://static.meiqia.com/dist/meiqia.js?_=t';
+        s.parentNode.insertBefore(j, s);
+})(window, document, 'script', '_MEIQIA');
+_MEIQIA('entId', 84164);
+_MEIQIA('showPanel');
